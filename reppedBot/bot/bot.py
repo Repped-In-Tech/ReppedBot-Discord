1805061a7910354619d4a887af51bc965357c77c
Justin Ferwerda <justin.ferwerda@gmail.com>
2024-04-12 11:27:45 -0500

    add client events into class

--- reppedBot/bot/bot.py
+++ reppedBot/bot/bot.py
@@ -2,14 +2,24 @@ import discord
 from discord.ext import commands
 from reppedBot import BotConfigMixin
 
-class ReppedBot(BotConfigMixin, commands.Bot):
-    def __init__(self,):
+class ReppedBot(BotConfigMixin, commands.Bot, discord.Client):
+    """
+    base class to interact with Discord server. Event listeners are defined within the class, commands are defined separately. 
+    """
+    def __init__(self):
         intents = discord.Intents.default()
+        intents.members = True
         intents.message_content = True
         command_prefix = '!'
         super().__init__(intents=intents, command_prefix=command_prefix)
 
+    async def on_member_join(self, member):
+        await member.create_dm()
+        await member.dm_channel.send(
+            f'Hi {member.name}, you successfully joined my test server for ReppedBot!'
+        )
+
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
-            await ctx.send(error)
+            await ctx.send("Command not recognized")
     
