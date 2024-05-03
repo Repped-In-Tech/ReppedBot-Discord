8fdfb9ef10c53f46e1f47c9746aa8cd2de6ae065
Justin Ferwerda <justin.ferwerda@gmail.com>
2024-05-03 10:38:41 -0500

    moved all setup to config class

--- reppedBot/config/setup.py
+++ reppedBot/config/setup.py
@@ -1,9 +1,17 @@
 import os
 from dotenv import load_dotenv
+import discord
 
 class BotConfigMixin:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    """loader for environment variables for webhooks and ReppedBot"""
+    def __init__(self):
         load_dotenv()
         self.discord_token=os.getenv('DISCORD_TOKEN')
         self.webhook_url=os.getenv('WEBHOOK_URL')
+        self.guild_id=os.getenv('GUILD_ID')
+        self.command_prefix=os.getenv('COMMAND_PREFIX')
+        intents = discord.Intents.default()
+        for intent in os.getenv('INTENTS').split(','):
+            if hasattr(intents, intent):
+                setattr(intents, intent, True)
+        super().__init__(command_prefix=self.command_prefix, intents=intents)
